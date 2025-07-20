dbc55092b47a956ba89d54fe1ad7f9d85144dffd
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2025-07-20 10:19:00 +0530

    feat: include numbers property in getConfig function

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,5 +3,6 @@ import { ProviderConfig } from "../types";
 export function getConfig(env: Cloudflare.Env): ProviderConfig {
   return {
     provider: env.provider,
+    numbers: env.numbers.split(",").map((number) => number.trim()),
   };
 }
