ed8af0fe5325e425351e39723bcf381e22cb10aa
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2025-07-20 11:16:05 +0530

    feat: enhance ProviderConfig schema with validation for numbers and recording types

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,23 @@
 import type { Context } from "hono";
-import { url } from "inspector";
 import z from "zod";
 
 export type AppContext = Context<{ Bindings: Env }>;
 
 export const ProviderConfig = z.object({
-  provider: z.string(),
-  numbers: z.array(z.string()),
-  recording: z.object({
-    type: z.string(),
-    url: z.string().url().optional(),
-    text: z.string().optional(),
-    maxLength: z.number().optional(),
-  })
+  provider: z.enum(["twilio"]),
+  numbers: z.array(z.string().min(5, "Number must be at least 5 characters long")).nonempty("At least one number must be specified"),
+  recording: z.discriminatedUnion("type", [
+    z.object({
+      type: z.literal("url"),
+      url: z.string().url().nonempty("Recording URL must be a valid URL"),
+    }),
+    z.object({
+      type: z.literal("text"),
+      text: z.string().nonempty("Recording text must be provided"),
+    })
+  ]).and(z.object({
+    maxLength: z.number().optional()
+  }))
 })
 
 export type ProviderConfigType = z.infer<typeof ProviderConfig>;
